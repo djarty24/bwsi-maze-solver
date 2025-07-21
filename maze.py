82b1c2fa44f7f7cbe727d18b7433cef7d0fda052
natnuo <natnu212@gmail.com>
2025-07-21 13:04:38 -0400

    Added fitness.py, maze.py, .gitignore

---
+++ maze.py
@@ -0,0 +1,15 @@
+class Maze:
+  def __init__(sideLength: int):
+    pass
+
+  """
+  Classically builds the maze with its given features
+  """
+  def build():
+    pass
+
+  """
+  Outputs the completed individual register
+  """
+  def get_path_superposition():
+    pass
